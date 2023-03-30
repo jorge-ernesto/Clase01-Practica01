d35b0deed450dd1d8afd38518ab35a759a0731c1
Ernesto <jorge.cywdt@gmail.com>
2023-03-30 02:19:02 -0500

    Cambios realizados por SistemasUNI
    ---- Gitignore ----
    
    - Gitignore for a Django project
    https://djangowaves.com/tips-tricks/gitignore-for-a-django-project/
    
    - Permitimos SQLite3 en Gitignore

--- Practica1/urls.py
+++ Practica1/urls.py
@@ -1,7 +1,7 @@
-"""Practica01 URL Configuration
+"""Practica1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Practica1.views import saludo, despedida, damefecha, calculaedad
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
+    path('despedida/', despedida),
+    path('damefecha/', damefecha),
+    path('calculaedad/<int:edad>/<int:agno>', calculaedad),
 ]
